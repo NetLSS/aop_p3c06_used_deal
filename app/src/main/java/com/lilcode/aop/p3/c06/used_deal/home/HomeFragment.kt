21f4582c3e673626e1eed6330827087c35c43650
NetLSS <futureprogrammer@naver.com>
2021-06-18 14:21:22 +0900

    Feat: HomeFragment.kt 기본 구성 (상단 툴바, 구분선, 리사이클러뷰);

--- app/src/main/java/com/lilcode/aop/p3/c06/used_deal/home/HomeFragment.kt
+++ app/src/main/java/com/lilcode/aop/p3/c06/used_deal/home/HomeFragment.kt
@@ -1,7 +1,24 @@
 package com.lilcode.aop.p3.c06.used_deal.home
 
+import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.lilcode.aop.p3.c06.used_deal.R
+import com.lilcode.aop.p3.c06.used_deal.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    private var binding: FragmentHomeBinding? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Log.d("sslee", "onViewCreated")
+
+        val fragmentHomeBinding = FragmentHomeBinding.bind(view)
+        binding = fragmentHomeBinding
+
+
+    }
+
 }
